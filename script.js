00709601375ffb896a9b8645032b49ce654e53af
Ronaldo <ronaldoteran2407@gmail.com>
2025-03-22 14:38:29 -0500

    Use request animation frame to set the animations for the coin

--- script.js
+++ script.js
@@ -6,19 +6,24 @@ coin.addEventListener("click", flipCoin);
 flipButton.addEventListener("click", flipCoin);
 
 function flipCoin() {
-	coin.style.animation = "none";
-    void coin.offsetHeight; // Forces a reflow, so the coin has to be animated every time there is a result
-	Math.random() < 0.5 ? heads() : tails();
+    requestAnimationFrame(() => {
+        coin.style.animation = "none";
+        requestAnimationFrame(() => {
+            Math.random() < 0.5 ? heads() : tails();
+        });
+    });
 }
 
 function heads() {
-	coin.style.animation = "coinAnimation 0.2s ease-in-out";
-	coin.src = "/resources/heads.svg";
-	result.innerText = "Heads";
+    setCoinState("Heads", "/resources/heads.svg");
 }
 
 function tails() {
-	coin.style.animation = "coinAnimation 0.2s ease-in-out";
-	coin.src = "/resources/tails.svg";
-	result.innerText = "Tails";
+    setCoinState("Tails", "/resources/tails.svg");
+}
+
+function setCoinState(outcome, imageSource) {
+    coin.style.animation = "coinAnimation 0.2s ease-in-out";
+    coin.src = imageSource;
+    result.textContent = outcome;
 }
