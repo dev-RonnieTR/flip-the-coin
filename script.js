ae8bfc63ad634a16f53418ee20c64580413353c8
Ronaldo <ronaldoteran2407@gmail.com>
2025-03-22 12:56:34 -0500

    Add animation to the coin every time there is a result

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const coin = document.getElementById("coin");
-const result = document.getElementById('result');
+const result = document.getElementById("result");
 const flipButton = document.getElementById("flip-button");
 
 coin.addEventListener("click", flipCoin);
 flipButton.addEventListener("click", flipCoin);
 
 function flipCoin() {
-	const random = Math.random();
-	random < 0.5 ? heads() : tails();
+	coin.style.animation = "none";
+    void coin.offsetHeight; // Forces a reflow, so the coin has to be animated every time there is a result
+	Math.random() < 0.5 ? heads() : tails();
 }
 
 function heads() {
+	coin.style.animation = "coinAnimation 0.2s ease-in-out";
 	coin.src = "/resources/heads.svg";
-    result.innerText = 'Heads';
+	result.innerText = "Heads";
 }
 
 function tails() {
+	coin.style.animation = "coinAnimation 0.2s ease-in-out";
 	coin.src = "/resources/tails.svg";
-    result.innerText = 'Tails'
+	result.innerText = "Tails";
 }
