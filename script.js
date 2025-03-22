9969760a7da2fa1c7d5b5da1e22529695806c3bb
Ronaldo <ronaldoteran2407@gmail.com>
2025-03-22 11:00:09 -0500

    create flipCoin() function that determines whether the result is heads or tails

---
+++ script.js
@@ -0,0 +1,15 @@
+const coin = document.getElementById("coin");
+const flipButton = document.getElementById("flip-button");
+
+function flipCoin() {
+	const random = Math.random();
+    random < 0.5 ? heads() : tails()
+}
+
+function heads() {
+
+}
+
+function tails() {
+    
+}
